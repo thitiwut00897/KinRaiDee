554edbd47b03487e7d4ef7e785cb283933448dfd
thitiwut088 <thitiwut00897@gmail.com>
2022-01-27 19:49:54 +0700

    หน้าผัก, responsive card(All)

--- mobile/screen/detailvagetable.js
+++ mobile/screen/detailvagetable.js
@@ -2,28 +2,25 @@ import React from "react";
 import { View, Text, StyleSheet, Image} from "react-native";
 import styles from "../style/styles";
 
-const Vagetable = (props) => {
+const VagetableDetail = (props) => {
 return (
   <View style={styles.container}>
   <View style={styles.page}>
     <View style={{marginLeft:30, marginRight:30, paddingTop:10,}}>
-      <View><Text style={{color:'black', fontSize:24, fontWeight: 'bold'}}>Name Vagetable</Text></View>
+      <View><Text style={{color:'black', fontSize:24, fontWeight: 'bold'}}>กะเพรา</Text></View>
         <View style={{alignItems: 'center',}}>
-            <Image source={require('../assets/profilefacebook.jpg')} style={{height:135, width:135, borderRadius:100, margin:20, borderColor:'gray', borderWidth:1}}></Image>
+            <Image source={require('../assets/krapao.jpg')} style={{height:135, width:135, borderRadius:100, margin:20, borderColor:'gray', borderWidth:1}}></Image>
         </View>
-        <Text>Botanical(พฤภษศาสตร์): ****** </Text>
-        <Text>Common Name(ชื่อทั่วไป): ****** </Text>
-        <Text>Genus(ประเภท): ****** </Text>
+        <Text>Botanical(พฤภษศาสตร์): Ocimum tenuiflorum </Text>
+        <Text>Common Name(ชื่อทั่วไป): กะเพรา </Text>
+        <Text>Genus(ประเภท): Ocimum </Text>
         <View style={{height:10, borderTopWidth:1, borderColor:'gray', marginTop:10, marginBottom:10}}></View>
         <Text style={{fontWeight: 'bold'}}>Descriptions</Text>
-        <Text>************************************************************************************
-        *********************************************************************************************************
-        ******************************************************************************************************************************
-        *********************
+        <Text>ป็นไม้พุ่มเตี้ยความสูงประมาณ 1-3 ฟุต ต้นค่อนข้างแข็ง แตกกิ่งก้านสาขามาก ก้านเป็นขน ก้านใบยาว รูปใบเรียว โคนใบรูดในลักษณะเรียวปลายมนรอบขอบใบเป็นหยัก พื้นใบด้านหน้าสีเขียว หรือแดงแก่กว่าด้านหลัง ซึ่งมีกระดูกใบนูนเห็นได้ชัด ดอกออกเป็นช่อตั้งขึ้นคล้ายฉัตร ออกบริเวณปลายยอดและปลายกิ่ง ดอกย่อยมีขนาดเล็ก รูปคล้ายระฆัง กลีบดอกมีทั้งชนิดสีขาวลายม่วงแดงและสีขาว เมล็ดอยู่ภายในกลีบ กลีบเลี้ยงสีม่วง ผลแห้งแล้วแตกออก เมื่อเมล็ดแก่สีดำ เมื่อนำไปแช่น้ำเปลือกหุ้มเมล็ดพองออกเป็นเมือก
         </Text>
     </View>
   </View>
 </View>
 );
 };
-export default Vagetable;
+export default VagetableDetail;
--- mobile/screen/main.js
+++ mobile/screen/main.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { View, Text, StyleSheet, Image, Button} from "react-native";
 import { TextInput } from "react-native-gesture-handler";
+import ImagedCarouselCard from "react-native-imaged-carousel-card";
 import styles from "../style/styles";
 
 
@@ -24,41 +25,61 @@ return (
             
          
           <Button title="Create Recipe" onPress={()=> props.navigation.navigate('Createrecipe')} style={{}}></Button>
+
+        
+
           <Text style={{fontWeight:'bold'}}>New Post {props.navigation.getParam("Token")}</Text>
           {/* card */}
-          <View style={{flexDirection:'row', justifyContent:'center'}}>
-            <View style={{height:170, width:145, borderRadius:10, backgroundColor:'#EBEBEB', padding:5, marginRight:5}}>
-              <View style={{flexDirection:'row'}}>
-                <View style={{width:'85%', flexDirection:'row'}}>
-                  <View><Image source={require('../assets/profilefacebook.jpg')} style={{height:20, width:20, borderRadius:15}}></Image></View>
-                  <View style={{marginLeft:1}}>
-                    <Text style={{fontSize:7}}>Thitiwut.</Text>
-                    <Text style={{color:'gray',fontSize:6}}>sun 29 AUG 00:00</Text>
+          
+          <View style={{flexDirection:'row', justifyContent:'flex-start', flexWrap: 'wrap',}}>
+              <View style={{height:170, width:145, borderRadius:10, backgroundColor:'#EBEBEB', padding:5, marginRight:5, marginBottom:5}}>
+                <View style={{flexDirection:'row'}}>
+                  <View style={{width:'85%', flexDirection:'row'}}>
+                    <View><Image source={require('../assets/profilefacebook.jpg')} style={{height:20, width:20, borderRadius:15}}></Image></View>
+                    <View style={{marginLeft:1}}>
+                      <Text style={{fontSize:7}}>Thitiwut.</Text>
+                      <Text style={{color:'gray',fontSize:6}}>sun 29 AUG 00:00</Text>
+                    </View>
                   </View>
+                  <View style={{flexDirection:'row-reverse'}}><Image source={require('../assets/bookmark.png')} style={{height:20, width:20, tintColor:'#F06C6A'}}/></View>
                 </View>
-                <View style={{flexDirection:'row-reverse'}}><Image source={require('../assets/bookmark.png')} style={{height:20, width:20, tintColor:'#F06C6A'}}/></View>
+                <View style={{alignItems:'center'}}><Image source={require('../assets/fried-rice.jpg')} style={{height:80, width:80, margin:5, borderRadius:0}}/></View>
+                <Text style={{fontWeight:'bold', fontSize:9}}>สูตรทำข้าวผัด</Text>
+                <Text style={{fontSize:8}} numberOfLines={2}>1.ผัดหมูให้สุก.....asdasdasdasdasdadasdasdasdasdsdfsdfsdfsdfsdfsdfdsfdsfdsfdsfs</Text><Text onPress={()=>props.navigation.navigate('Reivewrecipe')} style={{fontSize:8, color:'blue'}}>อ่านเพิ่มเติม</Text>
               </View>
-              <View style={{alignItems:'center'}}><Image source={require('../assets/fried-rice.jpg')} style={{height:80, width:80, margin:5, borderRadius:0}}/></View>
-              <Text style={{fontWeight:'bold', fontSize:9}}>สูตรทำข้าวผัด</Text>
-              <Text style={{fontSize:8}} numberOfLines={2}>1.ผัดหมูให้สุก.....asdasdasdasdasdadasdasdasdasdsdfsdfsdfsdfsdfsdfdsfdsfdsfdsfs</Text><Text onPress={()=>props.navigation.navigate('Reivewrecipe')} style={{fontSize:8, color:'blue'}}>อ่านเพิ่มเติม</Text>
-            </View>
-            <View style={{height:170, width:145, borderRadius:10, backgroundColor:'#EBEBEB', padding:5,}}>
-              <View style={{flexDirection:'row'}}>
-                <View style={{width:'85%', flexDirection:'row'}}>
-                  <View><Image source={require('../assets/profilefacebook.jpg')} style={{height:20, width:20, borderRadius:15}}></Image></View>
-                  <View style={{marginLeft:1}}>
-                    <Text style={{fontSize:7}}>Thitiwut.</Text>
-                    <Text style={{color:'gray',fontSize:6}}>sun 29 AUG 00:00</Text>
+              
+              <View style={{height:170, width:145, borderRadius:10, backgroundColor:'#EBEBEB', padding:5,}}>
+                <View style={{flexDirection:'row'}}>
+                  <View style={{width:'85%', flexDirection:'row'}}>
+                    <View><Image source={require('../assets/profilefacebook.jpg')} style={{height:20, width:20, borderRadius:15}}></Image></View>
+                    <View style={{marginLeft:1}}>
+                      <Text style={{fontSize:7}}>Thitiwut.</Text>
+                      <Text style={{color:'gray',fontSize:6}}>sun 29 AUG 00:00</Text>
+                    </View>
                   </View>
+                  <View style={{flexDirection:'row-reverse'}}><Image source={require('../assets/bookmark.png')} style={{height:20, width:20, tintColor:'#F06C6A'}}/></View>
                 </View>
-                <View style={{flexDirection:'row-reverse'}}><Image source={require('../assets/bookmark.png')} style={{height:20, width:20, tintColor:'#F06C6A'}}/></View>
+                <View style={{alignItems:'center'}}><Image source={require('../assets/fried-rice.jpg')} style={{height:80, width:80, margin:5, borderRadius:0}}/></View>
+                <Text style={{fontWeight:'bold', fontSize:9}}>สูตรทำข้าวผัด</Text>
+                <Text style={{fontSize:8}} numberOfLines={2}>1.ผัดหมูให้สุก.....asdasdasdasdasdadasdasdasdasdsdfsdfsdfsdfsdfsdfdsfdsfdsfdsfs</Text><Text onPress={()=>props.navigation.navigate('Reivewrecipe')} style={{fontSize:8, color:'blue'}}>อ่านเพิ่มเติม</Text>
               </View>
-              <View style={{alignItems:'center'}}><Image source={require('../assets/fried-rice.jpg')} style={{height:80, width:80, margin:5, borderRadius:0}}/></View>
-              <Text style={{fontWeight:'bold', fontSize:9}}>สูตรทำข้าวผัด</Text>
-              <Text style={{fontSize:8}} numberOfLines={2}>1.ผัดหมูให้สุก.....asdasdasdasdasdadasdasdasdasdsdfsdfsdfsdfsdfsdfdsfdsfdsfdsfs</Text><Text onPress={()=>props.navigation.navigate('Reivewrecipe')} style={{fontSize:8, color:'blue'}}>อ่านเพิ่มเติม</Text>
+              <View style={{height:170, width:145, borderRadius:10, backgroundColor:'#EBEBEB', padding:5,}}>
+                <View style={{flexDirection:'row'}}>
+                  <View style={{width:'85%', flexDirection:'row'}}>
+                    <View><Image source={require('../assets/profilefacebook.jpg')} style={{height:20, width:20, borderRadius:15}}></Image></View>
+                    <View style={{marginLeft:1}}>
+                      <Text style={{fontSize:7}}>Thitiwut.</Text>
+                      <Text style={{color:'gray',fontSize:6}}>sun 29 AUG 00:00</Text>
+                    </View>
+                  </View>
+                  <View style={{flexDirection:'row-reverse'}}><Image source={require('../assets/bookmark.png')} style={{height:20, width:20, tintColor:'#F06C6A'}}/></View>
+                </View>
+                <View style={{alignItems:'center'}}><Image source={require('../assets/fried-rice.jpg')} style={{height:80, width:80, margin:5, borderRadius:0}}/></View>
+                <Text style={{fontWeight:'bold', fontSize:9}}>สูตรทำข้าวผัด</Text>
+                <Text style={{fontSize:8}} numberOfLines={2}>1.ผัดหมูให้สุก.....asdasdasdasdasdadasdasdasdasdsdfsdfsdfsdfsdfsdfdsfdsfdsfdsfs</Text><Text onPress={()=>props.navigation.navigate('Reivewrecipe')} style={{fontSize:8, color:'blue'}}>อ่านเพิ่มเติม</Text>
+              </View>
+            
             </View>
-          </View>
-
         </View>
       </View>
     </View>
