694a832b0f6f51995dcea49e8fa71dbe954b909e
pathomrat <pathomrat97@gmail.com>
2022-03-08 19:24:48 +0700

    :rocket: Add check userId when route to login page

--- web/src/containers/login.js
+++ web/src/containers/login.js
@@ -1,6 +1,14 @@
+import { useEffect } from "react";
 import LoginForm from "../components/LoginBox/LoginForm";
 
 const Login = () => {
+  useEffect(() => {
+    const userId = localStorage.getItem('userId');
+    if (userId) {
+      localStorage.removeItem('userId');
+    }
+  })
+
   return (
     <LoginForm />
   );
