baf5a9cf7bf95b12653502ab58e1eee95eccea34
pathomrat <pathomrat97@gmail.com>
2022-03-21 23:23:19 +0700

    Edit setUser from response data

--- web/src/components/LoginBox/LoginForm.js
+++ web/src/components/LoginBox/LoginForm.js
@@ -8,106 +8,119 @@ import useInput from "../../hooks/useInput";
 import { usersApi } from "../../api/usersApi";
 import { useSetRecoilState } from "recoil";
 import { userState } from "../../store/atom";
-import Swal from 'sweetalert2';
+import Swal from "sweetalert2";
 
-const LoginGrid = styled(Grid)` 
-    width: 100%;
-    height: 80vh;
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-    align-items: center;
-`
+const LoginGrid = styled(Grid)`
+  width: 100%;
+  height: 80vh;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+`;
 
 const LoginPaper = styled(Paper)`
-    height: 363px;
-    width: 453px;
-    border-radius: 10px;    
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-`
+  height: 363px;
+  width: 453px;
+  border-radius: 10px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
 
 const HeaderText = styled(Typography)`
   cursor: default;
   margin-bottom: 20px;
   margin-top: 20px;
-`
+`;
 
 const LoginForm = () => {
-    const history = useHistory();
-    const setUser = useSetRecoilState(userState);
-    const [showPassword, setShowPassword] = useState(true);
-    let formIsValid = false;
+  const history = useHistory();
+  const setUser = useSetRecoilState(userState);
+  const [showPassword, setShowPassword] = useState(true);
+  let formIsValid = false;
 
-    const {
-        value: enteredEmail,
-        isValid: enteredEmailIsValid,
-        hasError: enteredEmailHasError,
-        valueChangeHandler: emailChangeHandler,
-        inputBlurHandler: emailBlurHandler,
-        reset: resetEmailInput,
-    } = useInput(value => value.includes('@'));
+  const {
+    value: enteredEmail,
+    isValid: enteredEmailIsValid,
+    hasError: enteredEmailHasError,
+    valueChangeHandler: emailChangeHandler,
+    inputBlurHandler: emailBlurHandler,
+    reset: resetEmailInput,
+  } = useInput((value) => value.includes("@"));
 
-    const {
-        value: enteredPassword,
-        isValid: enteredPasswordIsValid,
-        hasError: enteredPasswordHasError,
-        valueChangeHandler: passwordChangeHandler,
-        inputBlurHandler: passwordBlurHandler,
-        reset: resetPasswordInput,
-    } = useInput(value => value.length >= 8);
+  const {
+    value: enteredPassword,
+    isValid: enteredPasswordIsValid,
+    hasError: enteredPasswordHasError,
+    valueChangeHandler: passwordChangeHandler,
+    inputBlurHandler: passwordBlurHandler,
+    reset: resetPasswordInput,
+  } = useInput((value) => value.length >= 8);
 
-    if (enteredEmailIsValid && enteredPasswordIsValid) {
-        formIsValid = true;
-    }
+  if (enteredEmailIsValid && enteredPasswordIsValid) {
+    formIsValid = true;
+  }
 
-    const handleSubmit = () => {
-        usersApi().loginByEmail(enteredEmail, enteredPassword).then((res) => {
-            setUser(res.data);
-            localStorage.setItem('userId', res.data[0]._id)
-            resetEmailInput();
-            resetPasswordInput();
-            history.push('/')
-        }).catch(err => {
-            console.log(err)
-            Swal.fire({
-                icon: 'error',
-                title: 'Cannot login',
-                text: err.response.data.message
-            })
-            resetEmailInput();
-            resetPasswordInput();
-        })
-    }
+  const handleSubmit = () => {
+    usersApi()
+      .loginByEmail(enteredEmail, enteredPassword)
+      .then((res) => {
+        setUser(res.data[0]);
+        localStorage.setItem("userId", res.data[0]._id);
+        resetEmailInput();
+        resetPasswordInput();
+        history.push("/");
+      })
+      .catch((err) => {
+        console.log(err);
+        Swal.fire({
+          icon: "error",
+          title: "Cannot login",
+          text: err.response.data.message,
+        });
+        resetEmailInput();
+        resetPasswordInput();
+      });
+  };
 
-    return (
-        <LoginGrid>
-            <LoginPaper elevation={10}>
-            <HeaderText variant='h4' component="div">Admin Panel Login</HeaderText>
-                <LoginInput
-                    label={'email'}
-                    value={enteredEmail}
-                    handleChange={emailChangeHandler}
-                    handleOnBlur={emailBlurHandler}
-                />
-                {enteredEmailHasError && <Typography variant='subtitle1' color='error'>Email Invalid!</Typography>}
-                <LoginInput
-                    label={'password'}
-                    value={enteredPassword}
-                    handleChange={passwordChangeHandler}
-                    handleOnBlur={passwordBlurHandler}
-                    showPassword={showPassword}
-                    handleClickShowPassword={() => setShowPassword(!showPassword)}
-                />
-                {enteredPasswordHasError && <Typography variant='subtitle1' color='error'>Password Invalid!</Typography>}
-                <LoginButton disabled={!formIsValid} onHandleSubmit={handleSubmit}>
-                    LOGIN
-                </LoginButton>
-            </LoginPaper>
-        </LoginGrid>
-    )
-}
+  return (
+    <LoginGrid>
+      <LoginPaper elevation={10}>
+        <HeaderText variant="h4" component="div">
+          Admin Panel Login
+        </HeaderText>
+        <LoginInput
+          label={"email"}
+          value={enteredEmail}
+          handleChange={emailChangeHandler}
+          handleOnBlur={emailBlurHandler}
+        />
+        {enteredEmailHasError && (
+          <Typography variant="subtitle1" color="error">
+            Email Invalid!
+          </Typography>
+        )}
+        <LoginInput
+          label={"password"}
+          value={enteredPassword}
+          handleChange={passwordChangeHandler}
+          handleOnBlur={passwordBlurHandler}
+          showPassword={showPassword}
+          handleClickShowPassword={() => setShowPassword(!showPassword)}
+        />
+        {enteredPasswordHasError && (
+          <Typography variant="subtitle1" color="error">
+            Password Invalid!
+          </Typography>
+        )}
+        <LoginButton disabled={!formIsValid} onHandleSubmit={handleSubmit}>
+          LOGIN
+        </LoginButton>
+      </LoginPaper>
+    </LoginGrid>
+  );
+};
 
 export default LoginForm;
--- web/src/components/Table/UserTable.js
+++ web/src/components/Table/UserTable.js
@@ -1,60 +1,75 @@
-import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from "@mui/material";
+import {
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Typography,
+} from "@mui/material";
 import { styled } from "@mui/system";
 import { useRecoilValue } from "recoil";
 import { userState } from "../../store/atom";
 import StatusButton from "../Button/StatusButton";
 
 const MainTable = styled(TableContainer)`
-margin: 36px 0 60px 0;
-background-color: #F7F7F7;
-border-radius: 10px;
-border: none;
-`
+  margin: 36px 0 60px 0;
+  background-color: #f7f7f7;
+  border-radius: 10px;
+  border: none;
+`;
 
 const Header = styled(TableRow)`
-border-bottom: 2px solid #000000;
-box-shadow: 0px 3px 3px rgba(0, 0, 0, 0.3);
-`
+  border-bottom: 2px solid #000000;
+  box-shadow: 0px 3px 3px rgba(0, 0, 0, 0.3);
+`;
 
 const UserTable = (props) => {
-    const userAdmin = useRecoilValue(userState);
-    const { users, handleDelete } = props
-    return (
-        <MainTable component={Paper} >
-            <Table sx={{ minWidth: 650 }} aria-label="simple table">
-                <TableHead>
-                    <Header>
-                        <TableCell><Typography variant="h6">First Name</Typography></TableCell>
-                        <TableCell align="left"><Typography variant="h6">Last Name</Typography></TableCell>
-                        <TableCell align="center"><Typography variant="h6">Manage</Typography></TableCell>
-                    </Header>
-                </TableHead>
-                <TableBody>
-                    {users.map((user) => (
-                        <TableRow
-                            key={user._id}
-                            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                        >
-                            <TableCell component="th" scope="row">
-                                {user.firstName}
-                            </TableCell>
-                            <TableCell align="left">{user.lastName}</TableCell>
-                            <TableCell align="center">
-                                <StatusButton
-                                    disabled={userAdmin._id === user._id}
-                                    id={user._id}
-                                    onClickButton={handleDelete}
-                                    color='error'
-                                >
-                                    Delete
-                                </StatusButton>
-                            </TableCell>
-                        </TableRow>
-                    ))}
-                </TableBody>
-            </Table>
-        </MainTable>
-    )
-}
+  const userAdmin = useRecoilValue(userState);
+  const { users, handleDelete } = props;
+  return (
+    <MainTable component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <Header>
+            <TableCell>
+              <Typography variant="h6">First Name</Typography>
+            </TableCell>
+            <TableCell align="left">
+              <Typography variant="h6">Last Name</Typography>
+            </TableCell>
+            <TableCell align="center">
+              <Typography variant="h6">Manage</Typography>
+            </TableCell>
+          </Header>
+        </TableHead>
+        <TableBody>
+          {users.map((user) => (
+            <TableRow
+              key={user._id}
+              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {user.firstName}
+              </TableCell>
+              <TableCell align="left">{user.lastName}</TableCell>
+              <TableCell align="center">
+                <StatusButton
+                  disabled={userAdmin._id === user._id}
+                  id={user._id}
+                  onClickButton={handleDelete}
+                  color="error"
+                >
+                  Delete
+                </StatusButton>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </MainTable>
+  );
+};
 
 export default UserTable;
